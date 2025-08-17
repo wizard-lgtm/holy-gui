1612f4b0dbc68055c3a96753485336fbed15de60
wizard-lgtm <rippiten23@gmail.com>
2025-08-17 20:53:51 +0300

    add colors

---
+++ src/lib/colors.h
@@ -0,0 +1,20 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#include <SDL3/SDL.h>
+
+// Basic color palette
+#define COLOR_BLACK   (SDL_Color){0, 0, 0, 255}
+#define COLOR_WHITE   (SDL_Color){255, 255, 255, 255}
+#define COLOR_RED     (SDL_Color){255, 0, 0, 255}
+#define COLOR_GREEN   (SDL_Color){0, 255, 0, 255}
+#define COLOR_BLUE    (SDL_Color){0, 0, 255, 255}
+#define COLOR_YELLOW  (SDL_Color){255, 255, 0, 255}
+#define COLOR_CYAN    (SDL_Color){0, 255, 255, 255}
+#define COLOR_MAGENTA (SDL_Color){255, 0, 255, 255}
+#define COLOR_ORANGE  (SDL_Color){255, 165, 0, 255}
+#define COLOR_PURPLE  (SDL_Color){128, 0, 128, 255}
+#define COLOR_GRAY    (SDL_Color){128, 128, 128, 255}
+#define COLOR_BROWN   (SDL_Color){139, 69, 19, 255}
+
+#endif // COLORS_H
