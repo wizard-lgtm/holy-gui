42a40210a0f213bff648d2a685a02bc27b65067e
wizard-lgtm <rippiten23@gmail.com>
2025-08-17 21:17:08 +0300

    Add button and rendering functionality to main application

--- src/main.c
+++ src/main.c
@@ -4,12 +4,59 @@
 #include <stdlib.h>
 
 typedef struct Widget {
-    // For now empty, can expand later
 } Widget;
 
+typedef struct Font {
+    // TODO! later
+};
+
+typedef struct Label{
+    // Font* font; Font TODO!
+    char* text;
+    SDL_Color color;
+    SDL_FRect* rect;
+};
+
+
 typedef struct Button {
-    // For now empty
+    // Label text;
+    SDL_Color bgcolor;
+    int x;
+    int y;
+    int w;
+    int h;
+    SDL_FRect* rect;
+
 } Button;
+
+Button* create_button(int w, int h, int x, int y, SDL_Color bgcolor){
+    
+    Button* b = malloc(sizeof(Button));
+    if (b==NULL){
+        //return error;
+    }
+
+    b->bgcolor = COLOR_BLUE;
+    b->x = x;
+    b->y = y;
+    b->w = w;
+    b->h = h;
+    
+    SDL_FRect* rect = malloc(sizeof(SDL_FRect));
+    rect->h = b->h;
+    rect->w = b->w;
+    rect->x = b->x;
+    rect->y = b->y;
+
+    b->rect = rect;
+    
+    return b;
+}
+void free_button(Button* b){
+    if (!b) return;
+    if (b->rect) free(b->rect);
+    free(b);
+}
 
 typedef struct {
     int window_w;
@@ -87,7 +134,27 @@ void free_app(App* app) {
 
 // util
 void _set_render_draw_color(SDL_Color color, SDL_Renderer* renderer){
-    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.g, color.a);
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+}
+
+void render_single_widget(App* app, Button* button){
+    if (!button || !button->rect){
+        printf("Button or button rect is not defined!");
+        return;
+    };
+
+    _set_render_draw_color(button->bgcolor, app->renderer);
+    SDL_RenderFillRect(app->renderer, button->rect);
+}
+
+void render_widgets(App* app){
+    // Example button
+    Button* b = create_button(200, 100, 300, 200, COLOR_BLUE);
+
+    // Render the button
+    render_single_widget(app, b);
+
+    // Free the button after rendering (or store it in app->widgets later)
 }
 
 void mainloop(App* app) {
@@ -107,8 +174,11 @@ void mainloop(App* app) {
     // Clear the renderer
     SDL_RenderClear(app->renderer);
 
+    render_widgets(app);
+
     // Present the renderer (update the window)
     SDL_RenderPresent(app->renderer);
+
 }
 
 int main(){
